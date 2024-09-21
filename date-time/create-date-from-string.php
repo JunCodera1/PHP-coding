975c39a92704929238bbc9c66f52bd0e3e77802c
JunCodera1 <minhtien0601200@gmail.com>
2024-09-21 11:08:59 +0700

    Some way using strtotime() Update

--- date-time/create-date-from-string.php
+++ date-time/create-date-from-string.php
@@ -3,7 +3,16 @@
     <body>
         <?php
             $d = strtotime("11:04am September 15 2024");
-            echo "Created a date " . date("Y-m-d h:i:sa", $d);
+            echo "Created a date " . date("Y-m-d h:i:sa", $d) . "<br>";
+
+            $d = strtotime("tomorrow");
+            echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+            $d = strtotime("next Saturday");
+            echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+            $d = strtotime("+ 3 Months");
+            echo date("Y-m-d h:i:sa", $d) . "<br>";
         ?>
     </body>
 </html>
