06b8b69fd265fb387423eb98b98f23ad61946fcc
JunCodera1 <minhtien0601200@gmail.com>
2024-09-21 10:47:21 +0700

    How to get date in php

---
+++ date-time/get-date.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <body>
+        <?php
+        // 3 Ways to get date
+            echo "Today is " . date("Y/m/d"). "<br>";
+            echo "Today is" . date("Y.m.d") . "<br>";
+            echo "Today is ". date("Y-m-d") . "<br>";
+            echo "Today is ". date("l");
+        ?>
+    </body>
+</html>
